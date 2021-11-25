8b0ef7f9fa6153ed65c1fafef31672b0c965392f
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-11-25 15:35:28 +0200

    Functional tema 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Te tog sa introduci dimensiunea vetorului: ");
+            int dimensiuneVector = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[dimensiuneVector];
+            int c = 0;
+
+            Console.WriteLine("Introdu elementele vectorului: ");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[i] == nums[j])
+                    {
+                        c++;
+                    }
+                }
+            }
+            Console.Write($"Numarul de perechi perfecte este = {c}");
         }
     }
 }
